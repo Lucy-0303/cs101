7de48377602e18332d15ef8af4e060dcf21eace7
cs101 <lucy20070303@gmail.com>
2025-09-18 15:43:49 +0800

    Create main4.c

---
+++ EX2/main4.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main() {
+int a = 10;
+
+    if (a == 10){
+        printf("a is nequa to 10\n");
+    }else if (a < 20){
+        printf("a is less than 10\n");
+    }else{
+        printf("a is not less than 10\n");
+    }
+    return 0;
+}
