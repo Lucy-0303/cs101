d43da3bf6594964d85622617490d04610d5bba5d
cs101 <lucy20070303@gmail.com>
2025-10-02 13:52:41 +0800

    Update main1.c

--- EX3/main1.c
+++ EX3/main1.c
@@ -1,1 +1,31 @@
+#include <stdio.h>
+#define ROWS 6
 
+void print_sp(int i, int n) {
+    int j;
+    for (j = 1; j <= n - i; j++) {
+        printf(" ");
+    }
+}
+
+void print_num(int n) {
+    int j;
+    for (j = 1; j <= n; j++) {
+        printf("%d", n);
+        if (j < n) {
+            printf(" "); 
+        }
+    }
+}
+
+int main() {
+    int i; 
+
+    for (i = 1; i <= ROWS; i++) {
+        print_sp(i, ROWS); 
+        print_num(i);
+        printf("\n");
+    }
+
+    return 0;
+}
